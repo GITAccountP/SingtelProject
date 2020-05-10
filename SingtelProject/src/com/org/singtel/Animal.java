a06b5145aecaff2e7b329188da49cb5ff09d2cd5
pc admin <pc admin@DESKTOP-A6B2UJ2>
2020-05-10 21:58:04 +0800

    First Commit

---
+++ SingtelProject/src/com/org/singtel/Animal.java
@@ -0,0 +1,8 @@
+package com.org.singtel;
+
+class Animal {
+	void walk() {
+		System.out.println("I am walking");
+	}
+
+}
---
+++ SingtelProject/src/com/org/singtel/Bird.java
@@ -0,0 +1,12 @@
+package com.org.singtel;
+
+class Bird extends Animal {
+	void fly() {
+		System.out.println("I am flying");
+	}
+
+	void sing() {
+		System.out.println("I am singing");
+	}
+
+}
